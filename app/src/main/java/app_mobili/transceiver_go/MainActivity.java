f0451bdb52ccaec8fd6a64a32ed1f05cc399b270
mettil13 <matildemariano01@gmail.com>
2023-06-20 15:55:43 +0200

    created android studio project

---
+++ app/src/main/java/app_mobili/transceiver_go/MainActivity.java
@@ -0,0 +1,14 @@
+package app_mobili.transceiver_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
