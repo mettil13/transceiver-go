e23a70b30fb21e4669968f2736bacffe5d46df04
mettil13 <matildemariano01@gmail.com>
2023-07-27 14:41:02 +0200

    just a little map

--- app/src/main/java/app_mobili/transceiver_go/MainActivity.java
+++ app/src/main/java/app_mobili/transceiver_go/MainActivity.java
@@ -1,14 +1,40 @@
 package app_mobili.transceiver_go;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mappaTestFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mappaTest);
+        if(mappaTestFragment != null) {
+            mappaTestFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(false)
+                .add(
+                        new LatLng(42, 11),
+                        new LatLng(42, 31),
+                        new LatLng(30, 21),
+                        new LatLng(42, 11)
+
+                ));
     }
 }
