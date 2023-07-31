8be7b3dc29c7b2eb07e1a6600a844331c1c5182b
Luizo <emanuele109@hotmail.it>
2023-07-31 13:38:18 +0200

    Untested class, gotta test this 👀

--- app/src/main/java/app_mobili/transceiver_go/NetworkSignalStrength.java
+++ app/src/main/java/app_mobili/transceiver_go/NetworkSignalStrength.java
@@ -1,20 +1,25 @@
 package app_mobili.transceiver_go;
 
 import android.content.Context;
+import android.os.Build;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 
-
 public class NetworkSignalStrength extends Sensor{
-    private Context context;
     private TelephonyManager telephonyManager;
+    private int lteSignalStrength = -1; // Default value when not available
+    private int umtsSignalStrength = -1; // Default value when not available
 
+
+    // constructor
     public NetworkSignalStrength(Context context) {
-        this.context = context;
+        Sensor.setContext(context);
         telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     }
 
+    // once we start monitoring the signal Strength, you can request
+    // LTE & UMTS info with the methods get*SignalStrength below
     public void startMonitoringSignalStrength() {
         // Register the PhoneStateListener to start receiving signal strength updates
         telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
@@ -22,19 +27,27 @@ public class NetworkSignalStrength extends Sensor{
 
     public void stopMonitoringSignalStrength() {
         // Unregister the PhoneStateListener to stop receiving signal strength updates
+        // LISTEN_NONE is deprecated, but we have to use it since we support API levels of 24+
+        // while this got deprecated in API 31
         telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
     }
 
+    public int getLteSignalStrength() {
+        return lteSignalStrength;
+    }
+
+    public int getUmtsSignalStrength() {
+        return umtsSignalStrength;
+    }
+
+    // PhoneStateListener deprecated in API 33, but still gotta use it for our supported API levels
     private final PhoneStateListener phoneStateListener = new PhoneStateListener() {
         @Override
         public void onSignalStrengthsChanged(SignalStrength signalStrength) {
             super.onSignalStrengthsChanged(signalStrength);
             // Get the signal strength values for LTE and UMTS networks
-            int lteSignalStrength = signalStrength.getLevel(); // For LTE
-            int umtsSignalStrength = signalStrength.getGsmSignalStrength(); // For UMTS
-
-
+            lteSignalStrength = signalStrength.getLevel(); // For LTE, 0-4 scale
+            umtsSignalStrength = signalStrength.getGsmSignalStrength(); // For UMTS
         }
     };
-}
 }
--- app/src/main/java/app_mobili/transceiver_go/Sensor.java
+++ app/src/main/java/app_mobili/transceiver_go/Sensor.java
@@ -1,11 +1,21 @@
 package app_mobili.transceiver_go;
 
-public class Sensor {
+import android.content.Context;
 
-    private float SensorValue;
+public class Sensor {
+    private static Context context;
+    protected float SensorValue;
 
     public Sensor () {
         SensorValue = 0;
+        this.setContext(getContext());
     }
 
+    public static Context getContext() {
+        return context;
+    }
+
+    public static void setContext(Context context) {
+        Sensor.context = context;
+    }
 }
