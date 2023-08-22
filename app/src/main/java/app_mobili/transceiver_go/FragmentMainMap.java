7a84a5ba8dbe3d6f55d187d64ff226ac69047b7a
mettil13 <matildemariano01@gmail.com>
2023-08-22 16:11:26 +0200

    optimized drawing function to be camera aware

--- app/src/main/java/app_mobili/transceiver_go/FragmentMainMap.java
+++ app/src/main/java/app_mobili/transceiver_go/FragmentMainMap.java
@@ -21,35 +21,17 @@ import com.google.android.gms.maps.model.VisibleRegion;
  * Use the {@link FragmentMainMap#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentMainMap extends Fragment implements OnMapReadyCallback {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class FragmentMainMap extends Fragment implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
+    protected GoogleMap map;
 
     public FragmentMainMap() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment mainMap.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FragmentMainMap newInstance(String param1, String param2) {
         FragmentMainMap fragment = new FragmentMainMap();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,10 +39,6 @@ public class FragmentMainMap extends Fragment implements OnMapReadyCallback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -86,11 +64,15 @@ public class FragmentMainMap extends Fragment implements OnMapReadyCallback {
             }
         }
         */
-        VisibleRegion viewPort = googleMap.getProjection().getVisibleRegion();
-        MapTile tile = new MapTile(10, 10, viewPort.nearLeft.latitude - viewPort.farLeft.latitude);
-        tile.drawTile(googleMap, 0xff000000,0x66000000);
+        map = googleMap;
+        map.setOnCameraMoveListener(this);
+    }
 
-        Toast toast = Toast.makeText(getActivity(), viewPort.nearLeft.latitude + " " + viewPort.farLeft.latitude, Toast.LENGTH_LONG);
-        toast.show();
+    @Override
+    public void onCameraMove() {
+        map.clear();
+        VisibleRegion viewPort = map.getProjection().getVisibleRegion();
+        MapTile tile = new MapTile(map.getCameraPosition().target.latitude, map.getCameraPosition().target.longitude, viewPort.farLeft.longitude - viewPort.farRight.longitude);
+        tile.drawTile(map, 0xff000000, 0x66000000);
     }
 }
