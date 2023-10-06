f57c5e454c3c280d1222ee1b49c54b24055a53c2
Luizo <emanuele109@hotmail.it>
2023-10-06 16:29:09 +0200

    Update FragmentSomethingElse.java

--- app/src/main/java/app_mobili/transceiver_go/FragmentSomethingElse.java
+++ app/src/main/java/app_mobili/transceiver_go/FragmentSomethingElse.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
