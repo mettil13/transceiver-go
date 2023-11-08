8c1a9c859d8654213d03370ff3111a23e3f1e08c
Luizo <emanuele109@hotmail.it>
2023-11-08 18:35:41 +0100

    added "feature"

--- app/src/main/java/app_mobili/transceiver_go/FragmentAccountPreferences.java
+++ app/src/main/java/app_mobili/transceiver_go/FragmentAccountPreferences.java
@@ -1,10 +1,12 @@
 package app_mobili.transceiver_go;
 
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+
 
 public class FragmentAccountPreferences extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
@@ -23,11 +25,41 @@ public class FragmentAccountPreferences extends PreferenceFragmentCompat impleme
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-
         // if the user changed account name, update the database name
         if (s.equals("account_name")) {
             final String new_name = sharedPreferences.getString("account_name","user");
+            String[] dbList = requireContext().databaseList();
+
+            //checking if two databases will merge with this change
+            for (String dbname : dbList) {
+                if (!dbname.endsWith("-wal") && !dbname.endsWith("-shm")) {
+                    if(dbname.equals(new_name) && !old_name.equals(new_name)) {
+                        showMergeDialog();
+                        return;
+                    }
+                }
+            }
+
             DatabaseImportExportUtil.changeDatabaseName(requireContext(),old_name, new_name);
+            old_name = new_name;
         }
+    }
+    private void showMergeDialog() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        final String new_name = prefs.getString("account_name","user");
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle(R.string.merge_msg_title)
+                .setMessage(R.string.merge_msg_text)
+                .setPositiveButton(R.string.yes, (dialog, which) -> {
+                    // change the db name and proceed with merge action
+                    DatabaseImportExportUtil.changeDatabaseName(requireContext(),old_name, new_name);
+                    old_name = new_name;
+                })
+                .setNegativeButton(R.string.cancel, (dialog, which) -> {
+                    // User canceled the request
+                    prefs.edit().putString("account_name", old_name).apply();
+                    dialog.dismiss();
+                })
+                .show();
     }
 }
--- app/src/main/java/app_mobili/transceiver_go/FragmentLayerSelector.java
+++ app/src/main/java/app_mobili/transceiver_go/FragmentLayerSelector.java
@@ -1,41 +1,29 @@
 package app_mobili.transceiver_go;
 
 import android.app.AlertDialog;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Xml;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.preference.CheckBoxPreference;
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
-import androidx.room.Database;
 import androidx.room.Room;
-import androidx.sqlite.db.SimpleSQLiteQuery;
-import androidx.sqlite.db.SupportSQLiteQuery;
 
 import org.xmlpull.v1.XmlPullParser;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class FragmentLayerSelector extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.layer_selector, rootKey);
-        PreferenceCategory category = (PreferenceCategory) findPreference("available_maps");
+        PreferenceCategory category = findPreference("available_maps");
         //Preference pref = (Preference) findPreference("my_database");
 
         XmlPullParser parser = getResources().getXml(R.xml.layer_preference_list);
@@ -51,12 +39,13 @@ public class FragmentLayerSelector extends PreferenceFragmentCompat {
         if (count != 0) {
 
             new Thread(() -> {
-                String[] dbList = getContext().databaseList();
+                String[] dbList = requireContext().databaseList();
                 for (String s : dbList) {
                     if (!s.endsWith("-wal") && !s.endsWith("-shm")) {
-                        Preference pref = new CheckBoxPreference(getContext(), attr);
+                        Preference pref = new CheckBoxPreference(requireContext(), attr);
                         pref.setKey(s);
                         pref.setTitle(s);
+                        assert category != null;
                         category.addPreference(pref);
                     }
                 }
@@ -66,44 +55,38 @@ public class FragmentLayerSelector extends PreferenceFragmentCompat {
 
 
         Preference deleteDBs = findPreference("delete_maps");
-        deleteDBs.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                new Thread(() -> {
-                    String[] dbList = getContext().databaseList();
-                    List<String> toDeleteList = new ArrayList<>();
-                    for (String s : dbList) {
-                        if (!s.endsWith("-wal") && !s.endsWith("-shm") && !PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(s, true)) {
-                            Log.println(Log.ASSERT, "", "I should delete " + s);
-                            toDeleteList.add(s);
-                        }
+        assert deleteDBs != null;
+        deleteDBs.setOnPreferenceClickListener(preference -> {
+            new Thread(() -> {
+                String[] dbList = requireContext().databaseList();
+                List<String> toDeleteList = new ArrayList<>();
+                for (String s : dbList) {
+                    if (!s.endsWith("-wal") && !s.endsWith("-shm") && !PreferenceManager.getDefaultSharedPreferences(requireContext()).getBoolean(s, true)) {
+                        Log.println(Log.ASSERT, "", "I should delete " + s);
+                        toDeleteList.add(s);
                     }
+                }
 
-                    getActivity().runOnUiThread(new Runnable() {
-                        public void run() {
+                requireActivity().runOnUiThread(() -> {
 
-                            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-                            builder.setTitle(R.string.delete_maps_dialog_title)
-                                .setMessage(getResources().getString(R.string.delete_maps_dialog_text) + "\n" + toDeleteList.toString())
-                                .setPositiveButton(R.string.yes, (dialog, which) -> {
-                                    for (String s : toDeleteList) {
-                                        SquareDatabase db = Room.databaseBuilder(getActivity(), SquareDatabase.class, s).addMigrations(SquareDatabase.migration).build();
-                                        DatabaseImportExportUtil.deleteDatabase(db, getContext());
-                                    }
-                                    onCreatePreferences(null, null);
-                                })
-                                .setNegativeButton(R.string.no, (dialog, which) -> {
-                                    dialog.dismiss();
-                                })
-                                .show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                    builder.setTitle(R.string.delete_maps_dialog_title)
+                        .setMessage(getResources().getString(R.string.delete_maps_dialog_text) + "\n" + toDeleteList.toString())
+                        .setPositiveButton(R.string.yes, (dialog, which) -> {
+                            for (String s : toDeleteList) {
+                                SquareDatabase db = Room.databaseBuilder(requireActivity(), SquareDatabase.class, s).addMigrations(SquareDatabase.migration).build();
+                                DatabaseImportExportUtil.deleteDatabase(db, requireContext());
+                            }
+                            onCreatePreferences(null, null);
+                        })
+                        .setNegativeButton(R.string.no, (dialog, which) -> dialog.dismiss())
+                        .show();
 
-                        }
-                    });
+                });
 
 
-                }).start();
-                return true;
-            }
+            }).start();
+            return true;
         });
     }
 }
